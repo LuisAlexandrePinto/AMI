9b6c421ed2fdcafc053fac24029638c7600db8e8
Luis <luispintoxxx@gmail.com>
2018-06-29 03:02:06 +0100

    Project Created
    Project created with libgdx
    
    Co-Authored-By: andre-luz <andre-luz@users.noreply.github.com>

---
+++ android/src/com/nightgames/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.nightgames;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.nightgames.FingerinMain;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FingerinMain(), config);
+	}
+}
